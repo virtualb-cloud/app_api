5e27b0bb451606cad05577ee041556c609019806
Bizhan Zahedi <79849714+bizhanzhd@users.noreply.github.com>
2023-02-16 18:03:06 +0100

    not tested

---
+++ insert_advisors.py
@@ -0,0 +1,208 @@
+from sqlalchemy import create_engine
+
+class Insert:
+
+    def __init__(self) -> None:
+        
+        # db connection
+        self.schema_name = "mainapp"
+        self.engine = create_engine("postgresql://postgres:!vbPostgres@virtualb-rds-mainapp.clg6weaheijj.eu-south-1.rds.amazonaws.com:5432/postgres")
+        
+
+    def insert_products_description(self, products:list):
+        
+        # prepare fixed query
+        query = f'''
+            INSERT INTO {self.schema_name}.hub_product(
+                product_id, name_, isin_code, bloomberg_id, currency
+            )
+            VALUES '''
+        
+        # add the data to query
+        for product in products:
+
+            product_id = product["id"]
+            
+            record = product["description"]
+
+            add_statement = f'''('{product_id}', '{record["name"]}', '{record["isin_code"]}',
+            '{record["bloomberg_id"]}', '{record["currency"]}'),'''
+            query = query + add_statement
+
+        # to exclude the last ","
+        query = query[:-1] + ";"
+
+        # execute the query
+        with self.engine.connect() as conn:
+            conn.execute(statement=query)
+
+        return True
+
+    def insert_products_cultures(self, products:list):
+
+        # fixed query
+        query = f'''
+        INSERT INTO {self.schema_name}.prod_cultures(
+            product_id, financial_horizon_index, financial_litteracy_index,
+            financial_experience_index, risk_propensity_index,
+            esg_propensity_index, sophisticated_instrument, 
+            marginality_index
+        )
+        VALUES '''
+        
+        keys_list = [
+            "financial_horizon_index", "financial_litteracy_index",
+            "financial_experience_index", "risk_propensity_index",
+            "esg_propensity_index", "sophisticated_instrument", "marginality_index"]
+
+        for product in products:
+
+            product_id = product["id"]
+
+            # check
+            if not "cultures" in product.keys(): product["cultures"] = {}
+
+            record = product["cultures"]
+            
+            # control keys
+            for key in keys_list:
+                if not key in record.keys(): record[key] = "NULL"
+
+            # add the data to query   
+            add_statement = f'''('{product_id}', {record["financial_horizon_index"]},
+                {record["financial_litteracy_index"]}, {record["financial_experience_index"]},
+                {record["risk_propensity_index"]}, 
+                {record["esg_propensity_index"]}, 
+                {record["sophisticated_instrument"]}, {record["marginality_index"]}
+            ),'''
+
+            query = query + add_statement
+        
+        # to exclude the last ","
+        query = query[:-1] + ";"
+
+        # execute the query
+        with self.engine.connect() as conn:
+            result = conn.execute(statement=query)
+
+        return True
+
+    def insert_products_assets(self, products:list):
+
+        # fixed query
+        query = f'''
+        INSERT INTO {self.schema_name}.prod_assets(
+            product_id, equity, balanced,
+            bond, real_estate,
+            commodities, money_market,
+            liquidity
+        )
+        VALUES '''
+
+        keys_list = [
+            "equity", "balanced",
+            "bond", "real_estate",
+            "commodities", "money_market",
+            "liquidity"
+        ]
+
+        for product in products:
+
+            product_id = product["id"]
+
+            # check
+            if not "assets" in product.keys(): product["assets"] = {}
+
+            record = product["assets"]
+            
+            # control keys
+            for key in keys_list:
+                if not key in record.keys(): record[key] = "NULL"
+
+            # add the data to query  
+            add_statement = f'''('{product_id}', {record["equity"]},
+                {record["balanced"]}, {record["bond"]},
+                {record["real_estate"]}, {record["commodities"]}, 
+                {record["money_market"]}, {record["liquidity"]}
+            ),'''
+
+            query = query + add_statement
+        
+        # to exclude the last ","
+        query = query[:-1] + ";"
+
+        # execute the query
+        with self.engine.connect() as conn:
+            result = conn.execute(statement=query)
+
+        return True
+
+    def insert_products_needs(self, products:list):
+        
+        # fixed query
+        query = f'''
+        INSERT INTO {self.schema_name}.prod_needs(
+            product_id, health_insurance_need, home_insurance_need, longterm_care_insurance_need, 
+            payment_financing_need, loan_financing_need, mortgage_financing_need, 
+            capital_accumulation_investment_need, capital_protection_investment_need,
+            retirement_investment_need, income_investment_need, 
+            heritage_investment_need, liquidity_investment_need
+        )
+        VALUES '''
+
+        keys_list = [
+            'health_insurance_need', 'home_insurance_need', 'longterm_care_insurance_need', 
+            'payment_financing_need', 'loan_financing_need', 'mortgage_financing_need', 
+            'capital_accumulation_investment_need', 'capital_protection_investment_need',
+            'retirement_investment_need', 'income_investment_need', 
+            'heritage_investment_need', 'liquidity_investment_need'
+        ]
+
+        for product in products:
+
+            product_id = product["id"]
+
+            # check
+            if not "needs" in product.keys(): product["needs"] = {}
+
+            record = product["needs"]
+            
+            # control keys
+            for key in keys_list:
+                if not key in record.keys(): record[key] = "NULL"
+
+            # add the data to query 
+            add_statement = f'''('{product_id}', {record["health_insurance_need"]},
+                {record["home_insurance_need"]}, {record["longterm_care_insurance_need"]},
+                {record["payment_financing_need"]}, {record["loan_financing_need"]}, 
+                {record["mortgage_financing_need"]}, {record["capital_accumulation_investment_need"]},
+                {record["capital_protection_investment_need"]},
+                {record["retirement_investment_need"]}, {record["income_investment_need"]}, 
+                {record["heritage_investment_need"]}, {record["liquidity_investment_need"]}
+            ),'''
+            query = query + add_statement
+
+        # to exclude the last ","
+        query = query[:-1] + ";"
+
+        # execute the query
+        with self.engine.connect() as conn:
+            conn.execute(statement=query)
+        
+        return True
+
+    def run(self, products:list):
+
+        self.insert_products_description(products=products)
+        print("description pushed")
+
+        self.insert_products_cultures(products=products)
+        print("cultures pushed")
+
+        self.insert_products_assets(products=products)
+        print("assets pushed")
+
+        self.insert_products_needs(products=products)
+        print("needs pushed")
+
+        return True
---
+++ update_controller.py
@@ -0,0 +1,290 @@
+from sqlalchemy import create_engine
+
+class Update_controller:
+
+    def __init__(self) -> None:
+
+        # object to control the sample
+        # before adding to enrichment db
+
+        # db connection
+        self.schema_name = "mainapp"
+        self.engine = create_engine("postgresql://postgres:!vbPostgres@virtualb-rds-mainapp.clg6weaheijj.eu-south-1.rds.amazonaws.com:5432/postgres")
+        
+        # product id
+        query = f'''
+        SELECT product_id
+        FROM {self.schema_name}.hub_product
+        '''
+    
+        response = self.engine.connect().execute(statement=query)
+        product_ids = response.fetchall()
+
+        self.product_ids = []
+        if product_ids == None: self.product_ids = []
+        else:
+            for item in product_ids:
+                self.product_ids.append(item[0])
+    
+    def first_necessary_keys_controller(self, product:dict):
+
+        # flag & errors
+        flag = True
+        errors = ""
+
+        # controll "id"
+
+        if not "id" in product.keys(): 
+            errors += "try sending an id for each record. " 
+            flag = False
+        
+        elif not product["id"] in self.product_ids:
+            flag = False
+            errors += f"product_id '{product['id']}' does not exist in db. " 
+
+        return flag, errors
+
+    def first_optional_keys_controller(self, product:dict):
+
+        flags = {
+            "description" : False,
+            "assets" : False,
+            "cultures" : False,
+            "needs" : False
+            } 
+
+        if "description" in product.keys(): flags["description"] = True
+        if "cultures" in product.keys(): flags["cultures"] = True
+        if "assets" in product.keys(): flags["assets"] = True
+        if "needs" in product.keys(): flags["needs"] = True
+
+        return flags
+        
+    def description_keys_controller(self, product:dict):
+        
+        # mandatory variables
+        optional_keys = [
+            "name", "isin_code", "bloomberg_id", "currency"
+        ]
+
+        # flag & errors
+        flag = True
+        errors = ""
+
+        if product["description"] == {}:
+            flag = False
+            errors += f"try sending at least one of '{optional_keys}' as description dictionary keys. "
+
+        for key in product["description"].keys():
+
+            if not key in optional_keys:
+                flag = False
+                errors += f"try sending only '{optional_keys}' as description dictionary keys. "
+
+            elif not type(product["description"][key]) in [str]:
+                flag = False
+                errors += "try sending a value in [0, 1] range as description dictionary values. "
+
+        return flag, errors
+
+    def cultures_controller(self, product:dict):
+        
+        # optional variables
+        optional_keys = [
+            "financial_horizon_index", "financial_litteracy_index", 
+            "financial_experience_index", "risk_propensity_index", 
+            "esg_propensity_index", "sophisticated_instrument", 
+            "marginality_index"
+        ]
+
+        # flag & errors
+        flag = True
+        errors = ""
+
+        if product["cultures"] == {}:
+            flag = False
+            errors += f"try sending at least one of '{optional_keys}' as cultures dictionary keys. "
+
+        for key in product["cultures"].keys():
+
+            if not key in optional_keys:
+                flag = False
+                errors += f"try sending only '{optional_keys}' as cultures dictionary keys. "
+
+            elif not type(product["cultures"][key]) in [int, float]:
+                flag = False
+                errors += "try sending a value in [0, 1] range as cultures dictionary values. "
+
+            elif (product["cultures"][key] < 0) | (product["cultures"][key] > 1): 
+                flag = False
+                errors += f"try sending a value in [0, 1] range as cultures dictionary values. "
+
+        return flag, errors
+
+    def assets_controller(self, product:dict):
+        
+        # optional variables
+        optional_keys = [
+            "equity", "balanced", "bond",
+            "real_estate", "commodities", "money_market",
+            "liquidity"
+        ]
+
+        # flag & errors
+        flag = True
+        errors = ""
+
+        if product["assets"] == {}:
+            flag = False
+            errors += f"try sending at least one of '{optional_keys}' as assets dictionary keys. "
+
+        sum = 0
+        for key in product["assets"].keys():
+
+            if not key in optional_keys:
+                flag = False
+                errors += f"try sending only '{optional_keys}' as assets dictionary keys. "
+
+            elif not type(product["assets"][key]) in [int, float]:
+                flag = False
+                errors += "try sending a value in [0, 1] range as assets dictionary values. "
+
+            elif (product["assets"][key] < 0) | (product["assets"][key] > 1): 
+                flag = False
+                errors += f"try sending a value in [0, 1] range as assets dictionary values. "
+
+            else:
+                sum += product["assets"][key]
+        
+        # last check
+        if sum != 1:
+            flag = False
+            errors += f"assets should sum up to one. "
+
+        return flag, errors
+            
+    def needs_controller(self, product:dict):
+        
+        # optional variables
+        optional_keys = [
+            "capital_accumulation_investment_need", "capital_protection_investment_need", 
+            "liquidity_investment_need", "income_investment_need",
+            "retirement_investment_need", "heritage_investment_need",
+            "health_incurance_need", "home_insurance_need",
+            "longterm_care_insurance_need", "payment_financing_need",
+            "loan_financing_need", "mortgage_financing_need"
+        ]
+
+        # flag & errors
+        flag = True
+        errors = ""
+
+        if product["needs"] == {}:
+            flag = False
+            errors += f"try sending at least one of '{optional_keys}' as needs dictionary keys. "
+
+        for key in product["needs"].keys():
+
+            if not key in optional_keys:
+                flag = False
+                errors += f"try sending only '{optional_keys}' as needs dictionary keys. "
+
+            elif not type(product["needs"][key]) in [int, float]:
+                flag = False
+                errors += "try sending a value in [0, 1] range as needs dictionary values. "
+
+            elif (product["needs"][key] < 0) | (product["needs"][key] > 1): 
+                flag = False
+                errors += f"try sending a value in [0, 1] range as needs dictionary values. "
+            else:
+                sum += product["needs"][key]
+                
+        # last check
+        if sum != 1:
+            flag = False
+            errors += f"needs should sum up to one. "
+
+        return flag, errors
+
+    def run(self, products:list):
+
+        # flag & errors
+        flag = True
+        errors = ""
+
+        if type(products) != list:
+            flag = False
+            errors += "try sending a body [{rescord1}, ..., {rescordn}]. "
+            return flag, errors
+        
+        for product in products:
+            
+            if type(product) != dict:
+                flag = False
+                errors += "try sending a body [{rescord1}, ..., {rescordn}]. "
+                return flag, errors
+            
+            all_keys = ["id", "description", "cultures", "assets", "needs"]
+            for key in product.keys():
+                if not key in all_keys:
+                    flag = False
+                    errors += f"try sending only '{all_keys}' as record dictionary keys. "
+                    return flag, errors
+                
+            # necessary keys
+            flag, errs = self.first_necessary_keys_controller(product=product)
+            if not flag: 
+                errors += errs
+                return flag, errors
+            else:
+                product_id = product["id"]
+
+            # optional keys
+            flags = self.first_optional_keys_controller(product=product)
+            
+            sign = 0
+            for k, v in flags.items():
+                if v == True: sign = 1
+            
+            if sign == 0:
+                flag = False
+                errors = f"try sending at least one of '{all_keys[1:]}'. "
+                errors +=  f"error seen at id = '{product_id}'. "
+                return flag, errors
+
+            # control description if exists
+            if flags["description"]:
+
+                # control description keys
+                flag, errors = self.description_keys_controller(product=product)
+                if not flag: 
+                    errors += errs 
+                    errors +=  f"error seen at id = '{product_id}'. "
+                    return flag, errors
+
+
+            # control cultures if exists
+            if flags["cultures"]:
+                flag, errors = self.cultures_controller(product=product)
+                if not flag: 
+                    errors += errs
+                    errors +=  f"error seen at id = '{product_id}'. "
+                    return flag, errors
+
+            # control assets if exists
+            if flags["assets"]:
+                flag, errors = self.assets_controller(product=product)
+                if not flag: 
+                    errors += errs
+                    errors +=  f"error seen at id = '{product_id}'. "
+                    return flag, errors
+
+            # control needs if exists
+            if flags["needs"]:
+                flag, errors = self.needs_controller(product=product)
+                if not flag: 
+                    errors += errs
+                    errors +=  f"error seen at id = '{product_id}'. "
+                    return flag, errors
+
+        return flag, errors
