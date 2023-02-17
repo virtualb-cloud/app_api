a239469b4072d8e583f9215bf351d23933445a4e
Bizhan Zahedi <79849714+bizhanzhd@users.noreply.github.com>
2023-02-17 10:36:34 +0100

    tested

--- insert_advisors.py
+++ insert_advisors.py
@@ -19,20 +19,32 @@ class Insert:
             )
             VALUES '''
         
+        keys_list = [
+            "classification_index", "qualification_index", "diversification_index"
+        ]
         # add the data to query
         for advisor in advisors:
-
-            advisor_id = advisor["id"]
             
-            if advisor["description"] == {} 
+            advisor_id = advisor["id"]
+
+            # check
+            if not "description" in advisor.keys(): advisor["description"] = {}
+            
             record = advisor["description"]
 
-            add_statement = f'''('{advisor_id}', '{record["classification_index"]}',
-            '{record["qualification_index"]}', '{record["diversification_index"]}'),'''
+            add_statement = f'''('{advisor_id}','''
+            # control keys
+            for key in keys_list:
+                if not key in record.keys(): 
+                    record[key] = "NULL"
+                    add_statement += f''' {record[key]},'''
+                else:
+                    add_statement += f''' '{record[key]}','''
+
             query = query + add_statement
 
         # to exclude the last ","
-        query = query[:-1] + ";"
+        query = query[:-1] + ");"
 
         # execute the query
         with self.engine.connect() as conn:
